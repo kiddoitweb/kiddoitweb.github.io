324d67d3ceff146f0fd4ca75b6c78be346cba0fc
Ahmad <ifireir@gmail.com>
2025-09-08 02:15:31 +0330

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  ssr: false,
+  app: {
+    baseURL: "kiddoitweb.github.io",
+  },
   ui: {
     colorMode: false,
   },
